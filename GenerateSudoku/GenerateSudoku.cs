d21fa8a1c5173cf0ab0e2dd241d9a6ebc46d69ac
Jesper <jesper.jansen@student.fontys.nl>
2021-11-08 10:21:37 +0100

    backup sudoku

--- GenerateSudoku/GenerateSudoku.cs
+++ GenerateSudoku/GenerateSudoku.cs
@@ -22,8 +22,106 @@ namespace GenerateSudoku
             }
             else
             {
+                Sudoku board = new Sudoku();
+                var list = new List<Square>{ 
+                //Start first row
+                new Square{ x = 0, y = 0, value = 1 },
+                new Square{ x = 1, y = 0, value = 0 },
+                new Square{ x = 2, y = 0, value = 7 },
+                new Square{ x = 3, y = 0, value = 0 },
+                new Square{ x = 4, y = 0, value = 0 },
+                new Square{ x = 5, y = 0, value = 0 },
+                new Square{ x = 6, y = 0, value = 9 },
+                new Square{ x = 7, y = 0, value = 8 },
+                new Square{ x = 8, y = 0, value = 5 },
+                //Start Second row
+                new Square{ x = 0, y = 1, value = 0 },
+                new Square{ x = 1, y = 1, value = 0 },
+                new Square{ x = 2, y = 1, value = 0 },
+                new Square{ x = 3, y = 1, value = 6 },
+                new Square{ x = 4, y = 1, value = 5 },
+                new Square{ x = 5, y = 1, value = 0 },
+                new Square{ x = 6, y = 1, value = 0 },
+                new Square{ x = 7, y = 1, value = 0 },
+                new Square{ x = 8, y = 1, value = 3 },
+                //start third row
+                new Square{ x = 0, y = 2, value = 3 },
+                new Square{ x = 1, y = 2, value = 0 },
+                new Square{ x = 2, y = 2, value = 0 },
+                new Square{ x = 3, y = 2, value = 1 },
+                new Square{ x = 4, y = 2, value = 0 },
+                new Square{ x = 5, y = 2, value = 0 },
+                new Square{ x = 6, y = 2, value = 0 },
+                new Square{ x = 7, y = 2, value = 6 },
+                new Square{ x = 8, y = 2, value = 0 },
+                //Start fourth row
+                new Square{ x = 0, y = 3, value = 0 },
+                new Square{ x = 1, y = 3, value = 0 },
+                new Square{ x = 2, y = 3, value = 0 },
+                new Square{ x = 3, y = 3, value = 0 },
+                new Square{ x = 4, y = 3, value = 7 },
+                new Square{ x = 5, y = 3, value = 0 },
+                new Square{ x = 6, y = 3, value = 0 },
+                new Square{ x = 7, y = 3, value = 0 },
+                new Square{ x = 8, y = 3, value = 4 },
+                //start fhith row
+                new Square{ x = 0, y = 4, value = 5 },
+                new Square{ x = 1, y = 4, value = 0 },
+                new Square{ x = 2, y = 4, value = 0 },
+                new Square{ x = 3, y = 4, value = 2 },
+                new Square{ x = 4, y = 4, value = 0 },
+                new Square{ x = 5, y = 4, value = 0 },
+                new Square{ x = 6, y = 4, value = 0 },
+                new Square{ x = 7, y = 4, value = 1 },
+                new Square{ x = 8, y = 4, value = 0 },
+                //Start sixth row
+                new Square{ x = 0, y = 5, value = 2 },
+                new Square{ x = 1, y = 5, value = 0 },
+                new Square{ x = 2, y = 5, value = 0 },
+                new Square{ x = 3, y = 5, value = 0 },
+                new Square{ x = 4, y = 5, value = 4 },
+                new Square{ x = 5, y = 5, value = 0 },
+                new Square{ x = 6, y = 5, value = 5 },
+                new Square{ x = 7, y = 5, value = 0 },
+                new Square{ x = 8, y = 5, value = 0 },
+                //start seventh row
+                new Square{ x = 0, y = 6, value = 0 },
+                new Square{ x = 1, y = 6, value = 5 },
+                new Square{ x = 2, y = 6, value = 0 },
+                new Square{ x = 3, y = 6, value = 4 },
+                new Square{ x = 4, y = 6, value = 0 },
+                new Square{ x = 5, y = 6, value = 0 },
+                new Square{ x = 6, y = 6, value = 8 },
+                new Square{ x = 7, y = 6, value = 0 },
+                new Square{ x = 8, y = 6, value = 0 },
+                //start eigth row
+                new Square{ x = 0, y = 7, value = 8 },
+                new Square{ x = 1, y = 7, value = 0 },
+                new Square{ x = 2, y = 7, value = 0 },
+                new Square{ x = 3, y = 7, value = 7 },
+                new Square{ x = 4, y = 7, value = 0 },
+                new Square{ x = 5, y = 7, value = 5 },
+                new Square{ x = 6, y = 7, value = 0 },
+                new Square{ x = 7, y = 7, value = 0 },
+                new Square{ x = 8, y = 7, value = 6 },
+                //Start last Row
+                new Square{ x = 0, y = 8, value = 0 },
+                new Square{ x = 1, y = 8, value = 0 },
+                new Square{ x = 2, y = 8, value = 6 },
+                new Square{ x = 3, y = 8, value = 9 },
+                new Square{ x = 4, y = 8, value = 0 },
+                new Square{ x = 5, y = 8, value = 8 },
+                new Square{ x = 6, y = 8, value = 3 },
+                new Square{ x = 7, y = 8, value = 5 },
+                new Square{ x = 8, y = 8, value = 7 }
+                };
+                foreach(Square square in list)
+                {
+                    square.id = createid(square.x, square.y);
+                }
+                board.squares = list.ToArray();
                 //TODO Generate the board by own code incase of failure
-                return null;
+                return board;
             }
         }
 
